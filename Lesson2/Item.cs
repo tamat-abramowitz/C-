8c4fa93aa2f25f969bc8f0b081f1b2a15497104e
תמר <Hand1e1@mbjcomp.org.il>
2025-09-16 20:20:02 +0300

    Add files via upload

---
+++ Lesson2/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    public class Item
+    {
+        public int price;
+        public int barcode;
+        public string name;
+        public int age;
+        public double wheight;
+
+
+        public void toString(int price,int barcode,string name,int weight)
+        {
+            Console.WriteLine(price);
+            Console.WriteLine(barcode);
+            Console.WriteLine(name);
+            Console.WriteLine(wheight);
+        }
+    }
+}
